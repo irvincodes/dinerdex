443b09892748e5cd81376c7cdd20f448c966288a
irvincodes <119061206+irvincodes@users.noreply.github.com>
2023-03-16 10:42:06 +0800

    edited middleware positioning in server

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set("trust proxy", 1);
 
+app.use(methodOverride("_method"));
 app.use(
   session({
     secret: "secret-code",
@@ -42,7 +43,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(methodOverride("_method"));
 
 app.use("/", indexRouter);
 app.use("/diners", dinersRouter);
